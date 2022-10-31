130340526c3292127a34bf217f83e5631ad32ef8
hiltpold <m.hiltpold@protonmail.ch>
2022-11-01 00:34:41 +0100

    added user info

---
+++ services/auth/handlers/userinfo.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hiltpold/lakelandcup-api-gateway/services/auth/pb"
+)
+
+func UserInfo(ctx *gin.Context, c pb.AuthServiceClient) {
+	token, _ := ctx.Cookie("lakelandcup_refresh_token")
+
+	if token == "" {
+		ctx.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	res, err := c.Validate(context.Background(), &pb.ValidateRequest{
+		Token:     token,
+		TokenType: "ACCESS_TOKEN",
+	})
+
+	if err != nil || res.Status != http.StatusOK {
+		ctx.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	ctx.Set("userId", res.UserId)
+
+	ctx.Next()
+}
